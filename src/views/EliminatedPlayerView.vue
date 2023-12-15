bd224d4fb2391981dbb9cc42567591c28466a5b0
Molly Börjes <mollyborjes@student-236-242.eduroam.uu.se>
2023-12-15 14:36:22 +0100

    winner view och bakgrundsfärger

--- src/views/EliminatedPlayerView.vue
+++ src/views/EliminatedPlayerView.vue
@@ -4,12 +4,11 @@
   <div class="topPage">
     <h1>{{ uiLabels.theEliminatedPlayer }}</h1><h2>{{ this.userName }}</h2>
 
-    <h3>{{ uiLabels.hasGreenFlag }}{{ this.greenFlag }}</h3>
+    <h1>{{ uiLabels.hasGreenFlag }}</h1><h2>{{ this.greenFlag }} </h2>
 
     <h4 v-if="availableLifeline">{{ uiLabels.changedMind }} </h4>
   </div>
 
-  <!-- {{ namn + greenFlag skicka från choose answerView mha id }} -->
 
   <div class="saveButton" v-on:click="savePlayer" v-if="availableLifeline">
     <button class="lifebouyButton">
@@ -96,9 +95,11 @@ export default {
 </script>
 <style>
 .eliminatedPlayerViewBody{
-  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' width='2560' height='1660' preserveAspectRatio='none' viewBox='0 0 2560 1660'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1090%26quot%3b)' fill='none'%3e%3crect width='2560' height='1660' x='0' y='0' fill='rgba(235%2c 133%2c 189%2c 1)'%3e%3c/rect%3e%3cpath d='M0%2c847.787C155.73%2c859.484%2c251.124%2c677.787%2c372.184%2c579.13C478.393%2c492.575%2c596.764%2c422.29%2c666.692%2c304.468C741.851%2c177.831%2c784.013%2c34.721%2c782.689%2c-112.534C781.236%2c-274.059%2c763.41%2c-447.771%2c658.82%2c-570.871C553.943%2c-694.308%2c385.812%2c-744.568%2c225.576%2c-768.24C78.736%2c-789.933%2c-59.931%2c-729.466%2c-205.099%2c-698.501C-363.914%2c-664.625%2c-544.079%2c-684.102%2c-666.901%2c-577.873C-794.484%2c-467.527%2c-856.246%2c-293.098%2c-868.356%2c-124.851C-880.057%2c37.713%2c-835.564%2c208.233%2c-732.577%2c334.557C-638.294%2c450.203%2c-460.11%2c440.18%2c-337.907%2c525.794C-207.847%2c616.912%2c-158.356%2c835.893%2c0%2c847.787' fill='%23dc298b'%3e%3c/path%3e%3cpath d='M2560 2522.934C2780.263 2532.3 2994.3 2738.908 3195.6059999999998 2649.022 3393.9809999999998 2560.445 3430.025 2296.091 3515.4210000000003 2096.326 3598.706 1901.499 3692.364 1709.691 3687.709 1497.8600000000001 3682.683 1269.146 3665.9790000000003 999.673 3488.16 855.744 3309.337 711.002 3039.6639999999998 841.103 2815.017 791.491 2612.535 746.774 2450.061 567.6980000000001 2243.113 580.7829999999999 2017.624 595.04 1790.272 694.07 1643.192 865.581 1496.36 1036.8029999999999 1444.219 1275.634 1453.994 1500.98 1462.903 1706.352 1608.842 1868.393 1694.7939999999999 2055.126 1780.4279999999999 2241.169 1778.0529999999999 2498.995 1958.21 2596.404 2139.65 2694.507 2353.922 2514.1710000000003 2560 2522.934' fill='%23fae1ef'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1090'%3e%3crect width='2560' height='1660' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e");
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' width='2560' height='1660' preserveAspectRatio='none' viewBox='0 0 2560 1660'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1014%26quot%3b)' fill='none'%3e%3crect width='2560' height='1660' x='0' y='0' fill='rgba(255%2c 96%2c 154%2c 1)'%3e%3c/rect%3e%3cpath d='M0%2c1026.973C227.278%2c1047.243%2c489.108%2c1181.744%2c670.477%2c1043.283C854.577%2c902.737%2c754.872%2c599.117%2c841.708%2c384.395C922.881%2c183.677%2c1184.363%2c48.357%2c1159.832%2c-166.759C1135.393%2c-381.069%2c863.323%2c-462.749%2c726.125%2c-629.191C584.095%2c-801.495%2c558.426%2c-1110.716%2c340.738%2c-1160.447C119.668%2c-1210.951%2c-39.8%2c-936.724%2c-250.241%2c-852.244C-432.62%2c-779.029%2c-668.388%2c-838.543%2c-807.334%2c-699.559C-947.422%2c-559.432%2c-971.14%2c-336.566%2c-963.727%2c-138.563C-957.019%2c40.607%2c-826.094%2c181.111%2c-768.012%2c350.739C-701.452%2c545.127%2c-751.994%2c794.39%2c-597.785%2c930.171C-443.154%2c1066.324%2c-205.215%2c1008.671%2c0%2c1026.973' fill='%23ff2876'%3e%3c/path%3e%3cpath d='M2560 2829.3779999999997C2773.9 2868.781 3002.632 2733.383 3143.11 2567.3360000000002 3274.057 2412.555 3221.65 2181.864 3273.601 1985.891 3314.674 1830.954 3411.1059999999998 1697.092 3416.277 1536.886 3422.14 1355.259 3392.815 1173.562 3304.958 1014.49 3203.589 830.953 3088.545 595.875 2880.818 567.396 2667.541 538.156 2523.523 784.155 2331.264 880.998 2182.61 955.877 2013.1219999999998 970.612 1880.5410000000002 1071.246 1724.354 1189.798 1558.471 1319.685 1504.833 1508.29 1448.826 1705.227 1470.768 1935.728 1586.693 2104.495 1698.542 2267.328 1941.905 2257.248 2100.34 2375.245 2276.654 2506.557 2343.798 2789.551 2560 2829.3779999999997' fill='%23ff98be'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1014'%3e%3crect width='2560' height='1660' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e");
    min-height: 100vh;
-  padding: 20px;
+  padding: 10px;
+  background-size: cover;
+  min-height: 100vh;
 }
 
 header {
@@ -107,35 +108,31 @@ header {
   font-variant: small-caps;
 }
 h1 {
-  padding-top: 1em;
   font-family: "Lilita One", sans-serif;
-  font-size: 35px;
-  color: black;
-
-}
-
-h3 {
-  font-family: "Lilita One", sans-serif;
-  font-size: 30px;
+  font-size: 25px;
   color:rgb(254, 221, 255);
+
 }
+
 h2 {
   font-family: "Lilita One", sans-serif;
-  font-size: 80px;
-  color:rgb(237, 93, 177);
+  font-size: 50px;
+  color:rgb(129, 0, 19)
 
 }
 
 
 .lifeBouyUsed{
   font-family: "Lilita One", sans-serif;
-  font-size: 30px;
   color:rgb(254, 221, 255);
   font-size: 20px;
 }
 
 h4{
   font-size: 25px;
+  color:rgb(254, 221, 255);
+  font-family: "Lilita One", sans-serif;
+
   margin-top: 1em;
 }
 
@@ -170,6 +167,8 @@ h4{
 }
 .theLifebouy {
   width: 6em;
+  
+
 }
 
 .nextQButton {
