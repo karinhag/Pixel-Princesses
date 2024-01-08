8e69d16ea689462f62ec725a33fdf0ed78b0143a
larssonhanna <hannalarsson01@hotmail.com>
2024-01-08 10:07:57 +0100

    IP fix

--- src/views/PollView.vue
+++ src/views/PollView.vue
@@ -52,7 +52,7 @@
 <script>
 import QuestionComponent from "@/components/QuestionComponent.vue";
 import io from "socket.io-client";
-const socket = io("localhost:3000");
+const socket = io(sessionStorage.getItem("dataServer"));
 export default {
   name: "PollView",
   components: {
